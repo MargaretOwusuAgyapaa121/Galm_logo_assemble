3df460f020772071dd104fa9e4d3ef9212f67f56
MargaretOwusuAgyapaa121 <margarermensah123@gmail.com>
2026-03-04 04:57:33 +0000

    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,8 @@ export default function Hero() {
 
       <div className="hero-content">
         <img src="/assets/img/icon4.png" alt="Church Logo" className="hero-logo" />
-        <h1>Glorious Anchor Life Ministry</h1>
+        <h1 className="main-title">Glorious Anchor of Life</h1>
+        <h2 className="hero-subtitle">  Logos Assembly</h2>
         <p>
           Anchored in Purpose. Elevated in Grace. 
           <br />
