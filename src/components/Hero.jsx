a620b8edbbfcecc6976e7118db0feeb5db76af18
MargaretOwusuAgyapaa121 <margarermensah123@gmail.com>
2026-03-04 05:21:03 +0000

    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,42 @@
+// export default function Hero() {
+//   return (
+//     <section className="hero">
+//       <div className="hero-overlay"></div>
+      
+
+//       <div className="hero-content">
+//         <img src="/assets/img/icon4.png" alt="Church Logo" className="hero-logo" />
+//         <h1 className="main-title">Glorious Anchor of Life</h1>
+//         <h2 className="hero-subtitle">  Logos Assembly</h2>
+//         <p>
+//           Anchored in Purpose. Elevated in Grace. 
+//           <br />
+//           Step Into Your Divine Calling.
+//         </p>
+
+//         <button
+//           onClick={() =>
+//             document.getElementById("form").scrollIntoView({ behavior: "smooth" })
+//           }
+//         >
+//           Become a Member
+//         </button>
+//       </div>
+//     </section>
+//   );
+// }
 export default function Hero() {
   return (
     <section className="hero">
       <div className="hero-overlay"></div>
-      
 
       <div className="hero-content">
         <img src="/assets/img/icon4.png" alt="Church Logo" className="hero-logo" />
-        <h1 className="main-title">Glorious Anchor of Life</h1>
-        <h2 className="hero-subtitle">  Logos Assembly</h2>
+        <h1 className="main-title">
+          <span className="line1">Glorious Anchor of Life</span>
+          <span className="line2">  -  Logos Assembly</span>
+        </h1>
+
         <p>
           Anchored in Purpose. Elevated in Grace. 
           <br />
