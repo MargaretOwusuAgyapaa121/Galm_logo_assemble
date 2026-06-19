fed395fef676b1a05506192cb41548935b3d8f2f
MargaretOwusuAgyapaa121 <margarermensah123@gmail.com>
2026-06-19 21:37:51 +0000

    update motivation font size

--- src/components/Motive.jsx
+++ src/components/Motive.jsx
@@ -7,14 +7,15 @@ export default function Motivation() {
         <p>
           Welcome to our church family! We’re glad you’ve joined us and look forward to growing together in faith. Our community is here to support and encourage you on your spiritual journey.
         </p>
+
         <p>
           Remember, you are valued and loved. We pray you find warmth, friendship, and inspiration as you start this new chapter with us.
         </p>
 
         <p>
           We want to walk with you in the journey of our Lord and master Jesus Christ. Blessings!
-
         </p>
+        
          <p>
          Help us to fill in your particulars and indicate how you would like to be contacted
         </p>
